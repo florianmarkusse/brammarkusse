ca4186edeaa4f19c22ce6c1325fcbf3753c99924
Florian Markusse <florian.markusse@booking.com>
2024-12-26 21:35:56 +0100

    current status

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const foregroundInput = document.getElementById("foreground-color");
 const foregroundText = document.getElementById("foreground-color-text");
+foregroundText.textContent = foregroundInput.value;
 foregroundInput.addEventListener("input", function (e) {
   console.log("setting foreground to: ", e.target.value);
   foregroundText.textContent = e.target.value;
@@ -7,6 +8,7 @@ foregroundInput.addEventListener("input", function (e) {
 });
 const backgroundInput = document.getElementById("background-color");
 const backgroundText = document.getElementById("background-color-text");
+backgroundText.textContent = backgroundInput.value;
 backgroundInput.addEventListener("input", function (e) {
   console.log("setting background to: ", e.target.value);
   backgroundText.textContent = e.target.value;
--- text.md
+++ text.md
@@ -1,2 +1,10 @@
 ik praat graag met je,
 maar het blijft tussen ons
+
+foreground: #d6d6d6
+background: #949995
+
+foreground: #d6d6d6
+background: #a5957e
+
+Oppervlakte: 100m2
